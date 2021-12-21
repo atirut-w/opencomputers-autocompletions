d5d59391a8ec478e9f1cefcfa1aaef7874d0d585
Atirut Wattanamongkol <atirut.wattanamongkol@gmail.com>
2021-12-21 16:07:47 +0700

    Update stuff

--- computer.lua
+++ computer.lua
@@ -1,2 +1,35 @@
 ---@diagnostic disable-next-line: lowercase-global
-component = {}
+computer = {}
+
+---@alias arch
+---| '"Lua 5.2"'
+---| '"Lua 5.3"'
+
+--- Get the current architecture.
+---@return arch
+---@nodiscard
+function computer.getArchitecture() end
+
+--- Set the current architecture and reboot the computer.
+---@param architecture arch
+function computer.setArchitecture(architecture) end
+
+--- Get the address of the filesystem component from which to try to boot first. *New since OC 1.3*.
+---
+--- **Note:** This function is currently implemented in Lua BIOS. Be aware that custom BIOSes might not implement this function.
+---@return string
+---@nodiscard
+function computer.getBootAddress() end
+
+--- Set the address of the filesystem component from which to try to boot first. Call with nil / no arguments to clear. New since OC 1.3.
+---
+--- **Note:** This function is currently implemented in Lua BIOS. Be aware that custom BIOSes might not implement this function.
+---@param address? string
+function computer.setBootAddress(address) end
+
+--- Tries to pull a signal from the queue, waiting up to the specified amount of time before failing and returning `nil`. If no timeout is specified waits forever.
+---
+--- The first returned result is the signal name, following results correspond to what was pushed in `pushSignal`, for example. These vary based on the event type. Generally it is more convenient to use `event.pull` from the event library. The return value is the very same, but the `event` library provides some more options.
+---@param timeout? number
+---@return string | nil, ...
+function computer.pullSignal(timeout) end
--- proxies.lua/gpu.lua
+++ proxies.lua/gpu.lua
@@ -1,5 +1,17 @@
 ---@class GPUProxy: ComponentProxy
+--- Gets the currently set resolution.
+---@field getResolution fun(): integer, integer
+---
+---
 --- Writes a string to the screen, starting at the specified coordinates. The string will be copied to the screen's buffer directly, in a single row. This means even if the specified string contains line breaks, these will just be printed as special characters, the string will not be displayed over multiple lines. Returns `true` if the string was set to the buffer, `false` otherwise.
 ---
 --- The optional fourth argument makes the specified text get printed vertically instead, if `true`.
 ---@field set fun(x: integer, y: integer, value: string, vertical?: boolean): boolean
+---
+---
+--- Copies a portion of the screens buffer to another location. The source rectangle is specified by the `x`, `y`, `width` and `height` parameters. The target rectangle is defined by `x + tx`, `y + ty`, `width` and `height`. Returns `true` on success, `false` otherwise.
+---@field copy fun(x: integer, y: integer, width: integer, height: integer, tx: integer, ty: integer): boolean
+---
+---
+--- Fills a rectangle in the screen buffer with the specified character. The target rectangle is specified by the `x` and `y` coordinates and the rectangle's `width` and `height`. The fill character `char` must be a string of length one, i.e. a single character. Returns `true` on success, `false` otherwise.
+---@field fill fun(x: integer, y: integer, width: integer, height: integer, char: string): boolean
